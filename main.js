6226d40114e639777191cb855da06c4eb21015d6
anika-ik <79695696+anika-ik@users.noreply.github.com>
2021-09-09 14:04:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+rightWristX= 0;
+leftWristX= 0;
+difference= 0;
+noseX= 0;
+noseY= 0;
+
+function preload(){
+
+}
+
+function setup(){
+    video= createCapture(VIDEO);
+    video.size(550, 500);
+    
+
+    canvas= createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function draw(){
+    background('#969A97');
+    document.getElementById("font_side").innerHTML= "width and height of the font will be= " + difference + "px";
+    fill('#F9009');
+    textSize(10);
+    square(noseX, noseY, difference);
+    
+}
+
+function modelLoaded(){
+    console.log("posenet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
+        console.log("noseX= " + noseX + "noseY= " + noseY);
+
+        leftWristX= results[0].pose.leftWrist.x;
+        rightWristX= results[0].pose.rightWrist.x;
+        difference= floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX= " + rightWristX + "difference= " + difference);
+    }
+
+}
